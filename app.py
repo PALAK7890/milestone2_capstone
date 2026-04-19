59559478034a77b929f875b315a70054c8b0748f
Palak <palakdahiya78@gmail.com>
2026-04-19 11:04:49 +0530

    Fix future import position

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-from __future__ import annotations
+
 
 import json
 import streamlit as st
